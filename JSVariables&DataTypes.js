9f5e95d2042eb0cc958e70831ea44f83384e9934
AjayYogi0215 <ajayshankar0215@gmail.com>
2024-02-01 08:23:02 +0530

    Push-Practise

---
+++ JSVariables&DataTypes.js
@@ -0,0 +1,25 @@
+//multiple line
+var x;      //declaration
+x=10;       //initialization
+
+//single statement
+var x=10;   // declaration + initialization
+
+//single statement - multiple variable
+var x=100,y=200;
+
+document.write(x)
+console.log(x)
+
+//multiple line declaration,initialization
+let x;
+x="welcome";
+
+//single statement - declaration + initialization
+let x ="welcome";
+console.log(x);
+
+//multiple variable in single line
+let x ="welcome", y=100;
+console.log(x);
+console.log(y);
---
+++ OperaterDemo.js
@@ -0,0 +1,47 @@
+let x=20,y=10;
+
+//Arithmetic Operator (+,-,*,**,/,%,++,--)
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x**y);
+console.log(x/y);
+console.log(x%y);
+
+/*x++;
+console.log(x);*/
+
+++x
+console.log(x);
+
+/*y--;
+console.log(y);*/
+
+--y
+console.log(y);
+
+//Assignment Operator (=,+=,-=,*=,/=,%=)
+
+X=100;
+y=50;
+
+//console(X=y);
+
+console.log(X+=y);
+console.log(X-=y);
+console.log(X*=y);
+console.log(X/=y);
+console.log(X%=y);
+
+//Relational/Comparison Operator (==,===,!=,!==,>,<,>=,<=,?)
+//Always Return true/false
+
+x=10;
+y=20;
+console.log(x>y);//false
+console.log(x<y);//true
+
+console.log(x>=y);//false
+console.log(x<=y);//true
+
